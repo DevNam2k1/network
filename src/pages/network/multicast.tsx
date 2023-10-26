44abf2df3dae6b0cdb3fa4090a5ff5e61df1cfaa
Your Name <your@email.address>
2023-10-26 16:17:39 +0700

    16:17 26/10/23

--- src/pages/network/multicast.tsx
+++ src/pages/network/multicast.tsx
@@ -8,7 +8,7 @@ const Page = () => {
   const { t } = useTranslation()
   return (
     <>
-      <Card title={t('IGMP_List')} type="inner" headStyle={{background:"linear-gradient(109.6deg, rgb(44, 83, 131), rgb(44, 83, 131) 18.9%, rgb(68, 124, 143), rgb(44, 83, 131) 91.1%, rgb(44, 83, 131))",color: 'white'}}>
+      <Card title={t('IGMP_List')} type="inner" headStyle={{background:"linear-gradient(45deg, #07117e, #07117e",color: 'white'}}>
         <div style={{ overflowX: 'auto' }}>
         <Multicast_table />
         </div>
